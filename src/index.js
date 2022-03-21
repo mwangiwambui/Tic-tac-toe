5edd9d437befd1ab8a718cd48f8ea85a647b008e
mwangiwambui <wambui54mwangi@gmail.com>
2022-03-21 22:41:12 +0300

    Updated Board

--- src/index.js
+++ src/index.js
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Board extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      squares : Array(9).fill(null),
+    }
+  }
   renderSquare(i){
-    return <Square value={i} />;
+    return <Square value={this.state.squares[i]}
+    onClick={() => this.handleClick(i)}
+     />;
   }
   render(){
     const status = 'Next player: X';
